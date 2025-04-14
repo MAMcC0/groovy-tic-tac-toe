9202ddaa492c5e403acf8622e4d5451adc7419d5
MAMcC0 <meredith.a.mccollum@gmail.com>
2025-04-13 19:24:40 -0700

    added message logic

--- script.js
+++ script.js
@@ -9,5 +9,22 @@ let gameState = ["","","","","","","","",""];
 let gameActive = true;
 
 const winConditions = [
-    []
-]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8], //Rows
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],//Columns
+    [0,4,8],
+    [2,4,6], //Diagonals
+
+];
+
+const winMessage = () => `Player ${currentPlayer} Wins!`;
+const drawMessage = () => `It's a Draw! Good Game!`;
+const currentPlayerMessage = () => `Player ${currentPlayer}'s Turn!`;
+
+
+statusDisplay.textContent = currentPlayerMessage();
+
+fun
